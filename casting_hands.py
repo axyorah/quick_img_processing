240d41d4a6688af597047b7ed5a958f4c65edd55
axyorah <elvira.bozileva@gmail.com>
2020-10-12 01:36:15 +0200

    mod: adjusted casteing_hands.py template to work with ssd mobilenet gesture detector using tf2 api

--- casting_hands.py
+++ casting_hands.py
@@ -1,57 +1,42 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Sep 23 21:27:23 2020
-on Windows: `facial` venv
 
-tutorial is taken from:
+template is taken from:
     https://www.learnopencv.com/training-a-custom-object-detector-with-dlib-making-gesture-controlled-applications/?ck_subscriber_id=546165186
 @author: axeh
 """
-
-import dlib
+import tensorflow as tf
 import cv2 as cv
-#import glob
 import os
-import sys
 import numpy as np
-import matplotlib.pyplot as plt
-#import pyautogui as pyg
-import shutil
 import argparse
 import time
 
-from utils.pattern_utils import Pattern, Polygon
 from utils.perlin_flow import PerlinFlow
-from utils.hand_utils import FistPatternEffect, HandPatternEffect, JutsuPatternEffect
+from utils.pattern_utils import Pattern, Polygon
+from utils.pattern_utils import FistPatternEffect, HandPatternEffect, JutsuPatternEffect
 
 #%% parse input data
 parser = argparse.ArgumentParser()
-parser.add_argument("--detectorpaths", 
+parser.add_argument("--detectordir", 
                     default=None)
 args = vars(parser.parse_args())
 
-if args["detectorpaths"] is None:
-    rootdir = "D:/code_stuff/python_stuff/courses/computer_vision/dnn/" \
-    if "win" in sys.platform else \
-    "/media/axeh/code_stuff/python_stuff/courses/computer_vision/dnn/"
-    
-    detectorpaths = [os.path.join(rootdir,f"hog_{dclass}_detector.svm")
-                     for dclass in ["hand", "fist", "teleportation_jutsu"]]    
+if args["detectordir"] is None:
+    DETECTOR_DIR = "./dnn/ssd_mobilenet_gesture_detector/"
 else:
-    detectorpaths = args["detectorpaths"].split(" ")
+    DETECTOR_DIR = args["detectordir"]
 
 #%% load detectors
-detectors = [dlib.fhog_object_detector(detectorpath)
-             for detectorpath in detectorpaths]
+tf.keras.backend.clear_session()
+detect_fn = tf.saved_model.load(DETECTOR_DIR)
 colors = [(np.random.randint(0,255),np.random.randint(0,255), np.random.randint(0,255))
-          for _ in range(len(detectors))]
-classes = ["_".join(detectorpath.replace(os.path.sep, "/")\
-                                .split("/")[-1].split("_")[1:-1]) 
-           for detectorpath in detectorpaths]
+          for _ in range(3)]
+classes = ["hand", "fist", "teleportation_jutsu"]
 
 #%% define class effects
-
-            
 fistpattern = FistPatternEffect()
 handpattern = HandPatternEffect()
 jutsupattern = JutsuPatternEffect()
@@ -59,9 +44,10 @@ jutsupattern = JutsuPatternEffect()
 
 #%% start video capture
 cv.namedWindow("frame")
+w,h = 640, 480
 
 # initialize web cam
-cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+cap = cv.VideoCapture(0)#, cv.CAP_DSHOW)
 
 # initially the size of the hand and its center's x coor will be at 0
 #(MR: we're using sliding windows...)
@@ -78,37 +64,44 @@ while True:
         break
     
     # flip the frame
+    frame = cv.resize(frame, (w,h))
     frame = cv.flip(frame, 1)
     
     # create a copy of the frame
     copy = frame.copy()
+
+    # convert to right format
+    input_tensor = np.expand_dims(frame, axis=0)
+    input_tensor = tf.convert_to_tensor(input_tensor, dtype=tf.uint8)
     
     # get detections
-    [detections, confidences, indices] = \
-        dlib.fhog_object_detector.run_multiple(
-                detectors, frame, upsample_num_times=1)
+    detections = detect_fn(input_tensor)
     
     jutsu_detected = False
-    for detection,confidence,idx in zip(detections, confidences, indices):
-        idx = int(idx)
-        #if confidence < 0.5:
-        #    continue
+    jutsu_pt1, jutsu_pt2 = (None,None), (None,None)
+    for box,clss,score in zip(detections["detection_boxes"][0], # 0-dim - batch
+                              detections["detection_classes"][0],
+                              detections["detection_scores"][0]):
+        box = box.numpy()
+        clss = clss.numpy().astype(np.uint32)# + label_id_offset
+        score = score.numpy()
+
+        # detections are sorted based on scores in descending order
+        if score < 0.5:
+            break
+
+        # get box coordinates
+        y1,x1,y2,x2 = (box*np.array([h,w,h,w])).astype(int)
         
-        x1 = int(detection.left())
-        y1 = int(detection.top())
-        x2 = int(detection.right())
-        y2 = int(detection.bottom())
-        
-        #cv.rectangle(frame, (x1,y1), (x2,y2), colors[int(idx)], 3)
-        if classes[idx] == "hand":
-            #show_hand_effect(frame, (x1,y1), (x2,y2))
+        # Recall: class indices start from `1` (`0` is reserved for background)
+        if classes[clss-1] == "hand":
             handpattern.draw_pattern(frame, (x1,y1), (x2,y2))
-        elif classes[idx] == "fist":
+        elif classes[clss-1] == "fist":
             fistpattern.draw_pattern(frame, (x1,y1), (x2,y2))            
-        elif classes[idx] == "teleportation_jutsu":
+        elif classes[clss-1] == "teleportation_jutsu":
             jutsu_detected = True
             justu_pt1, jutsu_pt2 = (x1,y1), (x2,y2)    
-    jutsupattern.draw_pattern(frame, jutsu_detected)
+    jutsupattern.draw_pattern(frame, jutsu_detected, jutsu_pt1, jutsu_pt2)
         
     # display 
     cv.imshow("frame", frame)
