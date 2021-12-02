0abe93edc77510e124660c827673f112663c99f9
axyorah <elvira.bozileva@gmail.com>
2021-12-02 20:46:53 +0100

    cleanup: added comments and type hinting

--- utils/hand_utils.py
+++ utils/hand_utils.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from types import FunctionType
+from typing import List, Tuple, Dict, Set, Optional, Union
+
 import numpy as np
 import cv2 as cv
 
@@ -14,11 +17,11 @@ class Observable:
 class Param:
     def __init__(
         self, 
-        name, 
-        val, 
-        min_val, 
-        max_val, 
-        effect_fun
+        name: str, 
+        val: Union[int,float], 
+        min_val: Union[int,float], 
+        max_val: Union[int,float], 
+        effect_fun: FunctionType
     ):
         self.name = name
         self.val = val
@@ -26,14 +29,33 @@ class Param:
         self.max_val = max_val
         self.effect_fun = effect_fun
 
-    def update(self, frame, param, amount_change):
+    def update(
+        self, 
+        frame: np.ndarray, 
+        param: 'Param', 
+        amount_change: Union[int, float]
+    ) -> None:
+        """
+        this function should be `subscribed` to 
+        `Button`'s events from observable `Button`;
+        not subscribing it a param instantiation
+        because single param observes several buttons,
+        so it's easier to arrange it from button's side
+        """
         if param == self:
             self.val += amount_change
             self.val = np.clip(self.val, self.min_val, self.max_val)
             self.effect_fun(frame, self.val)
 
 class Button(Observable):
-    def __init__(self, name, path, param, amount_change, persist=False):
+    def __init__(
+        self, 
+        name: str, 
+        path: str, 
+        param: Param, 
+        amount_change: Union[int, float], 
+        persist: bool = False
+    ):
         super().__init__()
         self.name = name
         self.path = path
@@ -47,7 +69,11 @@ class Button(Observable):
         self.load()
         self.change.append(self.param.update)
 
-    def load(self):
+    def load(self) -> None:
+        """
+        loads button from provided image path
+        and stores button image and mask refence
+        """
         comb = cv.imread(self.path, cv.IMREAD_UNCHANGED)
         comb = cv.cvtColor(comb, cv.COLOR_BGRA2RGBA)
         self.btn_ref = comb[:,:,:3]
@@ -55,7 +81,10 @@ class Button(Observable):
         self.btn = self.btn_ref.copy()
         self.mask = self.mask_ref.copy()
 
-    def add(self, frame):
+    def add(self, frame: np.ndarray) -> None:
+        """
+        adds `this` button to specified `frame`
+        """
         # resize if needed
         h,w,c = frame.shape
         if self.mask != (h,w):
@@ -66,8 +95,21 @@ class Button(Observable):
         bg = cv.bitwise_and(frame, frame, mask=cv.bitwise_not(self.mask))
         cv.add(bg, fg, dst=frame)
 
-    def resolve_overlap(self, frame, handmask, threshold=0.75):
-        overlap = cv.bitwise_and(handmask, handmask, mask=self.mask)
+    def resolve_overlap(
+        self, 
+        frame: np.ndarray, 
+        mask: np.ndarray, 
+        threshold: float = 0.75
+    ) -> None:
+        """
+        checks if `this` button overlaps with `mask`;
+        if overlap to button-mask ratio is above `threshold`
+        triggers button effect;
+        if this ratio is below `threshold` but `persist` flag
+        is set to `True`, still triggers the effect function
+        but doesn't modify value of triggered observer
+        """
+        overlap = cv.bitwise_and(mask, mask, mask=self.mask)
         if np.sum(overlap) / np.sum(self.mask) > threshold:
             self.change(frame, self.param, self.amount_change)
         elif self.persist:
