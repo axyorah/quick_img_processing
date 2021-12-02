fdb8435b88cf394c6631c9b9342e156814b00f79
axyorah <elvira.bozileva@gmail.com>
2021-12-02 23:11:06 +0100

    style: mild style change

--- utils/pattern_utils.py
+++ utils/pattern_utils.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 31 22:03:36 2020
-
-@author: axeh
-"""
+from typing import List, Tuple, Dict, Set, Optional, Union
 import os
 import cv2 as cv
 import numpy as np
@@ -20,16 +16,23 @@ def rotate(vecs: np.ndarray, angle: float):
         vecs: (N,2) array of vectors to be rotated stacked along the 0-axis
         angle: float: rotation angle in radians
     """
-    return np.dot(vecs, 
-                  np.array([[np.cos(angle), np.cos(np.pi/2 + angle)],
-                            [np.sin(angle), np.sin(np.pi/2 + angle)]]))
+    return np.dot(
+        vecs, 
+        np.array([
+            [np.cos(angle), np.cos(np.pi/2 + angle)],
+            [np.sin(angle), np.sin(np.pi/2 + angle)]
+        ])
+    )
     
 class Pattern:
-    def __init__(self, vertices: np.ndarray, 
-                       center: list or np.ndarray, 
-                       dist_from_center=0,
-                       perlin=None,
-                       perlin_idx=0):
+    def __init__(
+        self, 
+        vertices: np.ndarray, 
+        center: Union[List,np.ndarray], 
+        dist_from_center: float = 0,
+        perlin: Optional[np.ndarray] = None,
+        perlin_idx: int = 0
+    ):
         self.vertices = vertices # (N,2), for closed figures 1st and last rows are the same
         self.center = np.array(center) # coordinates of center [y,x]
         self.delta_center = np.array([[0,0]]) # displacement of the pattern's center compared to the prev step
@@ -51,20 +54,28 @@ class Pattern:
         self.vertices = self.vertices + self.center + self.c2p
         
     def initialize_dists(self):
-        self.dists0 = np.linalg.norm(self.vertices - self.center, axis=1, keepdims=True)
+        self.dists0 = np.linalg.norm(
+            self.vertices - self.center, 
+            axis=1, 
+            keepdims=True
+        )
         self.dists = self.dists0.copy()
         
     def get_center_displacement(self, center=None):
-        center = np.array(center if center is not None else self.center).\
-                 reshape(1,2)
+        center = np.array(
+            center if center is not None else self.center
+        ).reshape(1,2)
         self.delta_center = center - self.center
         self.center = center
         return self.delta_center
         
     def adjust_scale(self, scale=None):
         if scale is not None:  
-            self.dists = np.linalg.norm(self.vertices - self.center, 
-                                        axis=1, keepdims=True)
+            self.dists = np.linalg.norm(
+                self.vertices - self.center, 
+                axis=1, 
+                keepdims=True
+            )
             scale_relative = scale * self.dists0 / self.dists
             self.vertices = self.center + \
                 (self.vertices - self.center) * scale_relative 
@@ -78,16 +89,23 @@ class Pattern:
         return self.vertices     
     
     def rotate_vertices(self, angle, center=None):
-        self.center = np.array(center if center is not None else self.center)
+        self.center = np.array(
+            center if center is not None else self.center
+        )
         
-        self.vertices = rotate(self.vertices-self.center, angle) + \
-                        self.center
+        self.vertices = rotate(
+            self.vertices-self.center, angle
+        ) + self.center
+
         return self.vertices.astype(int)
    
     def update_vertices(self, center=None, scale=None):
         self.frame += 1
         angle = 2*np.pi/100 + \
-                1*self.perlin[self.perlin_idx, self.frame%self.perlin.shape[1]]
+                1*self.perlin[
+                    self.perlin_idx, 
+                    self.frame%self.perlin.shape[1]
+                ]
         
         self.adjust_scale(scale=scale)
         self.adjust_center(center=center)
@@ -96,17 +114,22 @@ class Pattern:
         return self.vertices.astype(int)
     
 class Polygon(Pattern):
-    def __init__(self, num_vertices: int,
-                       center: list or np.ndarray,                        
-                       rad: float,
-                       dist_from_center=0,
-                       perlin=None,
-                       perlin_idx=0):        
-        super().__init__(np.array([[0,0]]), # <-- ugly dummy vertices
-                         center,
-                         dist_from_center=dist_from_center,
-                         perlin=perlin,
-                         perlin_idx=perlin_idx)
+    def __init__(
+        self, 
+        num_vertices: int,
+        center: Union[List,np.ndarray],                        
+        rad: float,
+        dist_from_center: float = 0,
+        perlin: Optional[np.ndarray] = None,
+        perlin_idx: int = 0
+    ):        
+        super().__init__(
+            np.array([[0,0]]), # <-- ugly dummy vertices
+            center,
+            dist_from_center=dist_from_center,
+            perlin=perlin,
+            perlin_idx=perlin_idx
+        )
         self.num_vertices = num_vertices
         self.rad = rad
         self.initialize_vertices() 
@@ -115,13 +138,15 @@ class Polygon(Pattern):
         
     def initialize_vertices(self, angles=None):
         if angles is None:
-            angles = [2*np.pi/self.num_vertices * i 
-                      for i in range(self.num_vertices)] + [0]
+            angles = [
+                2*np.pi/self.num_vertices * i 
+                for i in range(self.num_vertices)
+            ] + [0]
 
-        self.vertices = np.concatenate([rotate(np.array([[0, self.rad]]), angle)
-                                        for angle in angles],
-                                       axis=0) + \
-                        self.center + self.c2p
+        self.vertices = np.concatenate([
+            rotate(np.array([[0, self.rad]]), angle)
+            for angle in angles
+        ], axis=0) + self.center + self.c2p
         
         return self.vertices
 
@@ -221,74 +246,119 @@ class HandPatternEffect:
             self.mtris + self.pents
             
         self.colors = \
-            [(int(64*i/self.n_tris + 15*(self.n_tris-i)/self.n_tris),
-              int(255*i/self.n_tris + 225*(self.n_tris-i)/self.n_tris),
-              int(0*i/self.n_tris + 192*(self.n_tris-i)/self.n_tris)) 
-            for i in range(self.n_tris)] + \
-            [(int(29*i/self.n_sq + 245*(self.n_sq-i)/self.n_sq),
-              int(230*i/self.n_sq + 157*(self.n_sq-i)/self.n_sq),
-              int(181*i/self.n_sq + 10*(self.n_sq-i)/self.n_sq)) 
-            for i in range(self.n_sq)] + \
-            [(int(225*i/self.n_hex + 227*(self.n_hex-i)/self.n_hex),
-              int(225*i/self.n_hex + 138*(self.n_hex-i)/self.n_hex),
-              int(9*i/self.n_hex + 10*(self.n_hex-i)/self.n_hex)) 
-            for i in range(self.n_hex)]   + \
-            [(221,157,9)]*self.n_circs + \
-            [(234,217,0)]*self.n_mtris + \
-            [(0,242,255)]*self.n_pents
+            [
+                (
+                    int(64*i/self.n_tris + 15*(self.n_tris-i)/self.n_tris),
+                    int(255*i/self.n_tris + 225*(self.n_tris-i)/self.n_tris),
+                    int(0*i/self.n_tris + 192*(self.n_tris-i)/self.n_tris)
+                ) for i in range(self.n_tris)
+            ] + \
+            [
+                (
+                    int(29*i/self.n_sq + 245*(self.n_sq-i)/self.n_sq),
+                    int(230*i/self.n_sq + 157*(self.n_sq-i)/self.n_sq),
+                    int(181*i/self.n_sq + 10*(self.n_sq-i)/self.n_sq)
+                ) for i in range(self.n_sq)
+            ] + \
+            [
+                (
+                    int(225*i/self.n_hex + 227*(self.n_hex-i)/self.n_hex),
+                    int(225*i/self.n_hex + 138*(self.n_hex-i)/self.n_hex),
+                    int(9*i/self.n_hex + 10*(self.n_hex-i)/self.n_hex)
+                ) for i in range(self.n_hex)
+            ]   + \
+                [(221,157,9)]*self.n_circs + \
+                [(234,217,0)]*self.n_mtris + \
+                [(0,242,255)]*self.n_pents
         
         
     def get_tris(self):
         tri_rad = 0.7 * self.scale0; # center of the circle that touches triangle vetices
-        perlin_ids = [i * self.perlin.shape[0]//self.n_tris 
-                      for i in range(self.n_tris)]
-        return [Polygon(3, self.center, tri_rad, 
-                        perlin=self.perlin, perlin_idx=perlin_ids[i]) 
-                for i in range(self.n_tris)]
+        
+        perlin_ids = [
+            i * self.perlin.shape[0]//self.n_tris 
+            for i in range(self.n_tris)
+        ]
+
+        return [
+            Polygon(
+                3, self.center, tri_rad, 
+                perlin=self.perlin, perlin_idx=perlin_ids[i]
+            ) for i in range(self.n_tris)
+        ]
         
     def get_squares(self):
         sq_rad = 1.1 * self.scale0
         perlin_ids = [np.random.choice(self.perlin.shape[0])] * self.n_sq
-        squares = [Polygon(4, self.center, (0.93 if i%2 else 1)*sq_rad, 
-                           perlin=self.perlin, perlin_idx=perlin_ids[i]) 
-                   for i in range(self.n_sq)]
+        
+        squares = [
+            Polygon(
+                4, self.center, (0.93 if i%2 else 1)*sq_rad, 
+                perlin=self.perlin, perlin_idx=perlin_ids[i]
+            ) for i in range(self.n_sq)
+        ]
+
         for i in range(0,self.n_sq,2):
             squares[i  ].rotate_vertices(i//2 * 2*np.pi/(self.n_sq//2) / 4)
             squares[i+1].rotate_vertices(i//2 * 2*np.pi/(self.n_sq//2) / 4)
+        
         return squares
     
     def get_hex(self):
         hex_rad = 1.40 * self.scale0
-        perlin_ids = [i * self.perlin.shape[0] // (self.n_hex//2) 
-                  for i in range(self.n_hex//2) for _ in range(2)]
-        return [Polygon(6, self.center, (0.93 if i%2 else 1)*hex_rad, 
-                        perlin=self.perlin, perlin_idx=perlin_ids[i]) 
-                for i in range(self.n_hex)]
+        
+        perlin_ids = [
+            i * self.perlin.shape[0] // (self.n_hex//2) 
+            for i in range(self.n_hex//2) for _ in range(2)
+        ]
+
+        return [
+            Polygon(
+                6, self.center, (0.93 if i%2 else 1)*hex_rad, 
+                perlin=self.perlin, perlin_idx=perlin_ids[i]
+            ) for i in range(self.n_hex)
+        ]
     
     def get_circle(self):
         circ_rad = [i * self.scale0 for i in [1.15, 1.40, 1.45]]
-        return [Polygon(18, self.center, circ_rad[i], perlin=self.perlin) 
-                for i in range(self.n_circs)]
+
+        return [
+            Polygon(18, self.center, circ_rad[i], perlin=self.perlin) 
+            for i in range(self.n_circs)
+        ]
         
     def get_minitris(self):
         mtri_rad = 0.2*self.scale0
         mtri_dist = 1.55 * self.scale0
-        mtris = [Polygon(3, self.center, mtri_rad, 
-                         dist_from_center=mtri_dist, perlin=self.perlin)
-                 for i in range(self.n_mtris)]
+        
+        mtris = [
+            Polygon(
+                3, self.center, mtri_rad, 
+                dist_from_center=mtri_dist, perlin=self.perlin
+            ) for i in range(self.n_mtris)
+        ]
+
         for i,mtri in enumerate(mtris):
-            mtri.rotate_vertices(i * 2*np.pi/self.n_mtris)  
+            mtri.rotate_vertices(i * 2*np.pi/self.n_mtris)
+
         return mtris
     
     def get_pents(self):        
         pent_rad = 0.3*self.scale0
-        pents = [Polygon(5, self.center, pent_rad, 
-                     perlin=self.perlin, 
-                     perlin_idx=np.random.choice(self.perlin.shape[0]))
-                 for i in range(self.n_pents)]    
+
+        pents = [
+            Polygon(
+                5, self.center, pent_rad, perlin=self.perlin, 
+                perlin_idx=np.random.choice(self.perlin.shape[0])
+            ) for i in range(self.n_pents)
+        ] 
+        
         for pent in pents:
-            pent.initialize_vertices(angles=[2*i * 2*np.pi/5 for i in range(6)])
+            pent.initialize_vertices(
+                angles=[2*i * 2*np.pi/5 for i in range(6)]
+            )
             pent.initialize_dists()
+
         return pents
     
     def draw_pattern(self, frame, pt1, pt2):
@@ -299,8 +369,13 @@ class HandPatternEffect:
         scale = max((x2-x1),(y2-y1))
         
         for i,pattern in enumerate(self.patterns):
-            vertices = pattern.update_vertices(center=center, scale=scale)
-            cv.polylines(frame, [vertices], True, self.colors[i], 2)
+            vertices = pattern.update_vertices(
+                center=center, 
+                scale=scale
+            )
+            cv.polylines(
+                frame, [vertices], True, self.colors[i], 2
+            )
             
 class JutsuPatternEffect:
     def __init__(self):
@@ -318,9 +393,12 @@ class JutsuPatternEffect:
         KABOOM_DIR = os.path.join("imgs", "kaboom") if os.path.isdir("imgs") \
             else os.path.join("..", "imgs", "kaboom")
         
-        combined = [cv.imread(os.path.join(KABOOM_DIR, f"transp_kaboom{i}.png"),
-                              cv.IMREAD_UNCHANGED)
-                    for i in range(1,20)]
+        combined = [
+            cv.imread(
+                os.path.join(KABOOM_DIR, f"transp_kaboom{i}.png"),
+                cv.IMREAD_UNCHANGED
+            ) for i in range(1,20)
+        ]
 
         self.kabooms = [comb[:,:,:3] for comb in combined]
         self.masks   = [comb[:,:,3] for comb in combined]        
@@ -342,10 +420,16 @@ class JutsuPatternEffect:
                 # without the fade we could've also use:
                 # cv.copyTo(kaboom, mask, frame)
                 # but to do the correct fading we need to use the intermediate `combined`:
-                fadedegree = max(0, self.fade - (self.duration - self.ongoingframe) + 1) / self.fade # fade of kaboom
+                fadedegree = max(
+                    0, 
+                    self.fade - (self.duration - self.ongoingframe) + 1
+                ) / self.fade # fade of kaboom
+
                 combined = frame.copy()
                 cv.copyTo(kaboom, mask, combined)
-                cv.addWeighted(combined, 1 - fadedegree, frame, fadedegree, 0, frame)
+                cv.addWeighted(
+                    combined, 1 - fadedegree, frame, fadedegree, 0, frame
+                )
 
                 # increment the kaboom animation frame count
                 self.ongoingframe += 1
@@ -379,8 +463,12 @@ class LightningPatternEffect:
         frame_bundles = [[] for _ in range(num_bundles)]
         for i in range(num_bundles):
             for j in frame_indices:
-                fullimname = os.path.join(LIGHTNING_DIR, f"lightning{i}-{j}.png")
-                fullmaskname = os.path.join(LIGHTNING_DIR, f"mask{i}-{j}.png")
+                fullimname = os.path.join(
+                    LIGHTNING_DIR, f"lightning{i}-{j}.png"
+                )
+                fullmaskname = os.path.join(
+                    LIGHTNING_DIR, f"mask{i}-{j}.png"
+                )
             
                 # read
                 frame = cv.imread(fullimname)
@@ -457,8 +545,8 @@ class LightningPatternEffect:
             self.ongoingframe += 1
         else:
             self.ongoingframe = 0
-  
-#%%    
+
+
 if __name__ == '__main__':
     w,h, rad = 640, 480, 50
     img = 255 * np.ones((h, w, 3), dtype=int)
